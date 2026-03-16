b8434818040fcc26d0ca1bc921c4b57360e314ac
Hugo Torquetti <112561756+TorksCoder@users.noreply.github.com>
2026-03-16 14:11:58 -0300

    Iniciando o projeto

---
+++ test/login.test.js
@@ -0,0 +1,21 @@
+const request = require('supertest');
+const { expect } = require('chai');
+
+describe('Login', () => {
+    describe('POST /login', () => {
+        it('Deve retornar 200 com token em sting usando credenciais válidas', async () => {
+            /*Requisição para a API*/
+            const resposta = await request('http://localhost:3000')
+                .post('/login') /*método e url*/
+                .set('Content-Type', 'application/json') /*Definição do cabeçalho como json*/
+                /*envio de credenciais válidas*/
+                .send({
+                    'username': 'julio.lima',
+                    'senha': '123456'
+                    })
+            
+            expect(resposta.status).to.equal(200); /*Teste do código de erro*/
+            expect(resposta.body.token).to.be.a('string'); /*Teste do tipo de dado recebido */
+        });
+    });
+});
