ea4bf4fdbabbbbf951a84f8ec7f3a56005be7913
Hugo Torquetti <112561756+TorksCoder@users.noreply.github.com>
2026-03-17 23:15:20 -0300

    Adicionando o hook de beforeEach para captura do token. Retirada de comentários do helper

--- helpers/autenticacao.js
+++ helpers/autenticacao.js
@@ -2,9 +2,8 @@ const request = require('supertest');
 
 const obterToken = async (usuario, senha) => {
     const respostaLogin = await request(process.env.BASE_URL)
-        .post('/login') /*método e url*/
-        .set('Content-Type', 'application/json') /*Definição do cabeçalho como json*/
-        /*envio de credenciais válidas*/
+        .post('/login')
+        .set('Content-Type', 'application/json') 
         .send({
             'username': usuario,
             'senha': senha
